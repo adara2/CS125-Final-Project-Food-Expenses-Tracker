cbdde8bc1167d2b06c4ce29f33cc15664ebbed1a
amishdara <adara2@illinois.edu>
2019-12-03 03:54:07 -0600

    made activities and xmls

--- app/src/main/java/com/example/cs125finalprojectfoodexpensestracker/MainActivity.java
+++ app/src/main/java/com/example/cs125finalprojectfoodexpensestracker/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.cs125finalprojectfoodexpensestracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 public class   MainActivity extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        button = (Button) findViewById(R.id.customizeBudget);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openBudgetCustomization();
+            }
+        });
+
+    }
+
+    public void openBudgetCustomization() {
+        Intent intent = new Intent(this, budget_customization.class);
+        startActivity(intent);
+
     }
 }
