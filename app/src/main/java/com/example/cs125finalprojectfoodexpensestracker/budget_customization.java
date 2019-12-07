71a4bd4c185ab24d6bff6a317725353d10f730c3
yashaddaganti151 <yashsa2@illinois.edu>
2019-12-06 18:13:37 -0600

    spinner - yash

--- app/src/main/java/com/example/cs125finalprojectfoodexpensestracker/budget_customization.java
+++ app/src/main/java/com/example/cs125finalprojectfoodexpensestracker/budget_customization.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.content.Intent;
+import android.widget.Spinner;
 
 
-public class budget_customization extends AppCompatActivity {
+public class budget_customization extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     private Button budgetCustomizationHome;
 
@@ -26,10 +29,25 @@ public class budget_customization extends AppCompatActivity {
             }
         });
 
+        Spinner timeframe = findViewById(R.id.timeframeSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.timeframe, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        timeframe.setAdapter(adapter);
+        timeframe.setOnItemSelectedListener(this);
     }
 
     public void openMainActivity() {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
